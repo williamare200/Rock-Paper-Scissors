079a2b929da319230580f9561c45df258ce7fe75
william arevalos <williamare200@gmail.com>
2025-02-21 22:37:15 -0300

    feat: function rock, paper, scissors computer choice

--- app.js
+++ app.js
@@ -12,12 +12,20 @@ console.log("hello")
 
 const rock = 0;
 const paper = 1;
-const sciccors = 2;
+const scissors = 2;
 
 // random number 
 // random int number between 1 and 3 
 
 function getComputerChoice() {
-    return  Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+    if (computerChoice === 0) {
+        return "rock";
+    } else if (computerChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
 }
 console.log(getComputerChoice());
