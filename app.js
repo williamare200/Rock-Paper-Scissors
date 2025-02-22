0efab91606bc1e3ad744dd0e912e2e50120b2470
william arevalos <williamare200@gmail.com>
2025-02-21 22:52:11 -0300

    Feat: function player choice(return r,p,s depending input player)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("hello")
 
-// getComputerChoice
+/* // getComputerChoice */
 
 // a function that randomly return rock paper scissors
 
@@ -29,3 +29,40 @@ function getComputerChoice() {
     }
 }
 console.log(getComputerChoice());
+
+
+/* // GetHumanChoice */
+
+// a function that takes the user choice and returns it
+
+// return one of the valid choice depending on what the user inputs 
+
+// get user input a valid choice
+// return a choice :rock paper scissors
+
+function GetHumanChoice() {
+    let HumanChoice = prompt("escriba una de las opciones 'rock', 'paper', 'scissors'", "");
+    if (HumanChoice === "rock") {
+        return "rock";
+    } else if (HumanChoice === "paper") {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+console.log(GetHumanChoice());
+
+/* // The players score variables */
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+// logic to play a single round 
+
+// function called playRoud 
+
+// takes human and computer choice as arguments,
+// plays a single round(analize choices)
+// increments the round winner's score
+// logs a winner announcement
